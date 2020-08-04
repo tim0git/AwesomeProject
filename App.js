221c18b98fa8a87a26f826f278dea4879ed3e7c3
Timn Doolan <tim.doolan@cdl.co.uk>
2020-08-04 10:39:07 +0100

    tab nav

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Hello from './components/Hello';
 import Details from './components/Details';
-// import // SafeAreaView,
-// // StyleSheet,
-// // // ScrollView,
-// // View,
-// // Text,
-// // StatusBar,
-// 'react-native';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => (
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Hello} />
-      <Stack.Screen name="Details" component={Details} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Hello} />
+      <Tab.Screen name="Details" component={Details} />
+    </Tab.Navigator>
   </NavigationContainer>
 );
 
