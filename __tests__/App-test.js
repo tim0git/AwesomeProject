c8ea81c292694db18f78f2d38060a3bdb95f6ded
Timn Doolan <tim.doolan@cdl.co.uk>
2020-08-04 16:36:53 +0100

    testing now working

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,9 +1,19 @@
 import 'react-native';
 import React from 'react';
+
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import App from '../App';
+import Hello from '../components/Hello';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Test React Navigator', () => {
+  jest.useFakeTimers();
+
+  it('renders correctly', () => {
+    renderer.create(<Hello />);
+  });
+
+  test('renders correctly', () => {
+    const tree = renderer.create(<Hello />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
